88ac30a1052fe295975c1d50fa4fce0d3721f254
Svetlin <svetlinvatev@gmail.com>
2025-02-21 13:56:06 +0200

    optimizing the program

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 750
-	var expectedReturnRate = 5.3
-	var years float64 = 10
+	const inflationRate = 6.5
+	investmentAmount := 1000.0
+	years := 10.0
+	expectedReturnRate := 5.3
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
