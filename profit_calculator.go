f9f54ead81fc924bbe774816948317a34913c024
Svetlin Vatev <svetlinvatev@gmail.com>
2025-02-25 21:14:49 +0200

    Refactor profit calculator to improve user input handling and encapsulate financial calculations in a separate function

--- profit_calculator.go
+++ profit_calculator.go
@@ -3,25 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
 
-	fmt.Print("Enter the revenue: ")
-	fmt.Scan(&revenue)
+	revenue := getUserInput("Enter the revenue: ")
+	expenses := getUserInput("Enter the expenses: ")
+	taxRate := getUserInput("Tax Rate: ")
 
-	fmt.Print("Enter the expenses: ")
-	fmt.Scan(&expenses)
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
+	fmt.Printf("%.1f\n", ebt)
+	fmt.Printf("%.1f\n", profit)
+	fmt.Printf("%.3f\n", ratio)
 
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
+	return ebt, profit, ratio
+}
 
-	fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Println(ratio)
-
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
 }
