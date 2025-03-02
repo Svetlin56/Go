87e738ca765b9a963bb5ddb8ae873be19d729786
Svetlin Vatev <svetlinvatev@gmail.com>
2025-03-03 00:16:28 +0200

    Implement note and todo functionality: add note creation, display, and save methods; introduce todo creation and save logic

--- main.go
+++ main.go
@@ -1,41 +1,72 @@
 package main
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+
+	"example.com/note/note"
+	"example.com/note/todo"
 )
 
 func main() {
-	title, content, err := getNoteData()
+	title, content := getNoteData()
+	todoText := getUserInput("Todo text: ")
+
+	todo, err := todo.New(todoText)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-}
 
-func getNoteData() (string, string, error) {
-	title, err := getUserInput("Note title: ")
+	userNote, err := note.New(title, content)
 
 	if err != nil {
-		return "", "", err
+		fmt.Println(err)
+		return
 	}
 
-	content, err := getUserInput("Note content: ")
+	todo.Display()
+	err = todo.Save()
 
 	if err != nil {
-		return "", "", err
+		fmt.Println("Saving todo failed")
+		return
 	}
-	return title, content, nil
+
+	fmt.Println("Todo saved successfully")
+
+	userNote.Display()
+	err = userNote.Save()
+
+	if err != nil {
+		fmt.Println("Saving note failed")
+		return
+	}
+	fmt.Println("Note saved successfully")
 }
 
-func getUserInput(prompt string) (string, error) {
-	fmt.Print(prompt)
-	var value string
-	fmt.Scanln(&value)
+func getNoteData() (string, string) {
+	title := getUserInput("Note title: ")
 
-	if value == "" {
-		return "", errors.New("Invalid input.")
+	content := getUserInput("Note content: ")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Printf("%v ", prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
 	}
 
-	return value, nil
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text
 }
